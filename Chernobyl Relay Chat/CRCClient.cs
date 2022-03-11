d9d1953fdb1a4f1f716b161922f344d9066f9337
itsAnchorpoint <talktoanchorpoint@gmail.com>
2022-03-11 21:31:17 +0300

    Fixed DeathMessages not showing up correctly for other players (needs testing)

--- Chernobyl Relay Chat/CRCClient.cs
+++ Chernobyl Relay Chat/CRCClient.cs
@@ -314,7 +314,7 @@ namespace Chernobyl_Relay_Chat
                 else if (CRCOptions.ReceiveDeath && (DateTime.Now - lastDeath).TotalSeconds > CRCOptions.DeathInterval)
                 {
                     lastDeath = DateTime.Now;
-                    nick = e.Data.Nick; //fakeNick;
+                    nick = fakeNick; //e.Data.Nick;
                 }
                 else
                     return;
