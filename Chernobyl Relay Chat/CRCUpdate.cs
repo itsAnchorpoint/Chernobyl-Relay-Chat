ada70b3b6d1571f2152646325c87cfac2547c6a2
itsAnchorpoint <talktoanchorpoint@gmail.com>
2022-04-26 07:03:13 +0300

    fixed a stoopid error popping up and disturbing my fellow GAMERS

--- Chernobyl Relay Chat/CRCUpdate.cs
+++ Chernobyl Relay Chat/CRCUpdate.cs
@@ -14,7 +14,7 @@ namespace Chernobyl_Relay_Chat
 
         public static bool CheckFirstUpdate()
         {
-            UpdateChecker updateChecker = new UpdateChecker("itsAnchorpoint", "Chernobyl-Relay-Chat-Rebirth", "0.4.0");
+            UpdateChecker updateChecker = new UpdateChecker("itsAnchorpoint", "Chernobyl-Relay-Chat-Rebirth", "0.4.1");
             UpdateType updateType;
             try
             {
@@ -51,7 +51,7 @@ namespace Chernobyl_Relay_Chat
 
         public static async Task<bool> CheckUpdate()
         {
-            UpdateChecker updateChecker = new UpdateChecker("itsAnchorpoint", "Chernobyl-Relay-Chat-Rebirth", "0.4.0");
+            UpdateChecker updateChecker = new UpdateChecker("itsAnchorpoint", "Chernobyl-Relay-Chat-Rebirth", "0.4.1");
             UpdateType updateType;
             try
             {
